57685c7a264a8682a9f6ac92bb3164cb30ebc2d7
Aleksandrs <aleksandrs.varakuta@chi.lv>
2025-08-05 15:24:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,11 @@ def index():
 @app.route('/files/<path:filename>')
 def serve_file(filename):
     return send_from_directory('static', filename)
-
+    
+@app.route('/.well-known/<path:filename>')
+def well_known_files(filename):
+    return send_from_directory('.well-known', filename)
+    
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
